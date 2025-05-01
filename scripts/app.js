9cb3a6137deeff8e71f3806505d9bfa4e174c93a
Ali Shahrabi <Ali88206497@gmail.com>
2025-05-01 23:04:07 +0330

    Improving the hamburger menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,28 @@
+// !hamburger-menu
 const hamburgerIcon = document.getElementById("hamburger-icon");
-const hamburgerContainer = document.getElementById("hamburger-container");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const closeIcon = document.querySelector("#hamburger-menu #menu-title img");
 const body = document.querySelector("body");
 const profile = document.getElementById("profile-container");
 const taskContainer = document.getElementById("task-container");
 const taskCompleted = document.getElementById("task-compeleted");
 const verticalLine = document.getElementById("vertical-line");
 hamburgerIcon.addEventListener("click", () => {
-    hamburgerContainer.classList.toggle("expand");
-    if (hamburgerContainer.classList.contains("expand")) {
-        hamburgerIcon.src = "./assets/images/mobile/light/x.svg";
-        body.style.backgroundColor = "gray";
-        profile.style.display = "none";
-        taskContainer.style.display = "none";
-        taskCompleted.style.display = "none";
-        verticalLine.style.display = "none";
-    } else {
-        hamburgerIcon.src = "./assets/images/mobile/light/menu.svg";
-        body.style.backgroundColor = "#fff";
-        profile.style.display = "flex";
-        taskContainer.style.display = "block";
-        taskCompleted.style.display = "block";
-        verticalLine.style.display = "block";
-    }
+    hamburgerMenu.classList.add("expand");
+    hamburgerMenu.style.display = "block";
+    body.style.backgroundColor = "gray";
+    profile.style.display = "none";
+    taskContainer.style.display = "none";
+    taskCompleted.style.display = "none";
+    verticalLine.style.display = "none";
+})
+
+closeIcon.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("expand");
+    hamburgerMenu.style.display = "none";
+    body.style.backgroundColor = "#fff";
+    profile.style.display = "flex";
+    taskContainer.style.display = "block";
+    taskCompleted.style.display = "block";
+    verticalLine.style.display = "block";
 })
