c48f78a033cff9cc9a020ab5be65453ee5a22699
Ali Shahrabi <Ali88206497@gmail.com>
2025-05-01 17:10:54 +0330

    Debugging hamburger menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,25 @@
-// !hamburger-menu
-const hamburgerImg = document.getElementById("hamburger");
-const hamburgerMenu = document.getElementById("hamburger-menu");
-hamburgerImg.addEventListener("click", () => {
-    hamburgerMenu.classList.toggle("expand");
-    if (hamburgerMenu.classList.contains("expand")) {
-        hamburgerImg.src = "./assets/images/mobile/light/x.svg";
+const hamburgerIcon = document.getElementById("hamburger-icon");
+const hamburgerContainer = document.getElementById("hamburger-container");
+const body = document.querySelector("body");
+const profile = document.getElementById("profile-container");
+const taskContainer = document.getElementById("task-container");
+const taskCompleted = document.getElementById("task-compeleted");
+const verticalLine = document.getElementById("vertical-line");
+hamburgerIcon.addEventListener("click", () => {
+    hamburgerContainer.classList.toggle("expand");
+    if (hamburgerContainer.classList.contains("expand")) {
+        hamburgerIcon.src = "./assets/images/mobile/light/x.svg";
+        body.style.backgroundColor = "gray";
+        profile.style.display = "none";
+        taskContainer.style.display = "none";
+        taskCompleted.style.display = "none";
+        verticalLine.style.display = "none";
     } else {
-        hamburgerImg.src = "./assets/images/mobile/light/menu.svg";
+        hamburgerIcon.src = "./assets/images/mobile/light/menu.svg";
+        body.style.backgroundColor = "#fff";
+        profile.style.display = "flex";
+        taskContainer.style.display = "block";
+        taskCompleted.style.display = "block";
+        verticalLine.style.display = "block";
     }
 })
