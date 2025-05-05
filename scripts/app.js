dfbb9d31855ff87048a55bf0435ab6a17af2f9fe
Ali Shahrabi <Ali88206497@gmail.com>
2025-05-05 13:54:33 +0330

    finishing hamburger menu of dark mode

--- scripts/app.js
+++ scripts/app.js
@@ -28,10 +28,69 @@ closeIcon.addEventListener("click", () => {
 })
 
 // !dark-mode
+const toggleContainer = document.getElementById("toggle");
+const toggleDark = document.getElementById("toggle-dark");
+const toggleLight = document.getElementById("toggle-light");
+const homeImg = document.querySelector("#side-bar li img[alt='home']");
+const taskImg = document.querySelector("#side-bar li img[alt='task']");
+const calendarImg = document.querySelector("#side-bar li img[alt='calendar']");
+const reportImg = document.querySelector("#side-bar li img[alt='report']");
+const settingImg = document.querySelector("#side-bar li img[alt='setting']");
+const supportImg = document.querySelector("#side-bar li img[alt='support']");
+const profileImg = document.querySelector("#side-bar li img[alt='profile']");
+const lightImg = document.querySelector("#side-bar #toggle img[alt='light']");
+const darkImg = document.querySelector("#side-bar #toggle img[alt='dark']");
+const logoutImg = document.querySelector("#log-out img");
 
-const darkBtn = document.getElementById("dark");
-const darkMode = function () {
-    document.documentElement.classList.toggle("dark");
+const lightMode = function () {
+    document.documentElement.classList.remove("dark");
+    document.documentElement.classList.add("light");
+
+    toggleContainer.style.backgroundColor = "#F7F7F7";
+
+    if (document.documentElement.classList.contains("light")) {
+        toggleContainer.classList.remove("dark");
+        toggleContainer.classList.add("light");
+        closeIcon.src = "assets/images/mobile/light/x.svg";
+        homeImg.src = "assets/images/mobile/light/home.svg";
+        taskImg.src = "assets/images/mobile/light/task-square.svg";
+        calendarImg.src = "assets/images/mobile/light/calendar.svg";
+        reportImg.src = "assets/images/mobile/light/report.svg";
+        settingImg.src = "assets/images/mobile/light/setting.svg";
+        supportImg.src = "assets/images/mobile/light/24-support.svg";
+        profileImg.src = "assets/images/mobile/light/profile.svg";
+        lightImg.src = "assets/images/mobile/light/sun.svg";
+        darkImg.src = "assets/images/mobile/light/moon.svg";
+        logoutImg.src = "assets/images/mobile/light/logout.svg";
+        body.style.backgroundColor = "gray";
+    }
 }
 
-darkBtn.addEventListener("click", darkMode);
+const darkMode = function () {
+    document.documentElement.classList.add("dark");
+    document.documentElement.classList.remove("light");
+    if (document.documentElement.classList.contains("dark")) {
+        toggleContainer.classList.add("dark");
+        toggleContainer.classList.remove("light");
+
+        toggleContainer.style.backgroundColor = "#0C1B31";
+        closeIcon.src = "assets/images/mobile/dark/close.svg";
+        homeImg.src = "assets/images/mobile/dark/home.svg";
+        taskImg.src = "assets/images/mobile/dark/task-square.svg";
+        calendarImg.src = "assets/images/mobile/dark/calendar.svg";
+        reportImg.src = "assets/images/mobile/dark/report.svg";
+        settingImg.src = "assets/images/mobile/dark/setting.svg";
+        supportImg.src = "assets/images/mobile/dark/24-support.svg";
+        profileImg.src = "assets/images/mobile/dark/profile.svg";
+        lightImg.src = "assets/images/mobile/dark/sun.svg";
+        darkImg.src = "assets/images/mobile/dark/moon.svg";
+        logoutImg.src = "assets/images/mobile/dark/logout.svg";
+        body.style.backgroundColor = "#000";
+    }
+
+}
+
+
+
+toggleDark.addEventListener("click", darkMode);
+toggleLight.addEventListener("click", lightMode);
